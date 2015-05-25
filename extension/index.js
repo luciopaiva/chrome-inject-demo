67fb5452dae2fd295b1dd32d17c92e41102c0b85
Lucio Paiva <luciopaiva@gmail.com>
2015-05-24 21:28:28 -0300

    Improve injection engine

--- extension/index.js
+++ extension/index.js
@@ -6,9 +6,14 @@
 // @version        0.1.0
 // ==/UserScript==
 
-console.info('Script running...');
-
 $(function () {
-    $('<script>').attr('src', chrome.extension.getURL('jquery.min.js')).appendTo('body'); //.on('load', removeWhenDone);
-    $('<script>').attr('src', chrome.extension.getURL('inject.js')).appendTo('body'); //.on('load', removeWhenDone);
+
+    console.info('Script running...');
+
+    function loadExtensionScript(src) {
+        $('<script>').attr('src', chrome.extension.getURL(src)).appendTo('body').remove();
+    }
+
+    loadExtensionScript('jquery.min.js');
+    loadExtensionScript('inject.js');
 });
